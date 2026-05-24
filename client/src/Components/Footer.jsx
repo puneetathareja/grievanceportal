16695fa8e01fec246b7b8779abcd64901f84391a
puneetathareja <puneetathareja@gmail.com>
2026-05-24 19:15:29 +0530

    Finalize project: commit remaining changes

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -71,7 +71,7 @@ export default function footer(props) {
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-white sm:text-center dark:text-gray-400">
-              © 2023{" "}
+              © 2026{" "}
               <a href="" className="hover:underline">
                 आवाज़™
               </a>
